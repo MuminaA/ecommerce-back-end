b93e08206773de12206ae89540c014db7c09a0f5
Mumina <muminaabdi13@gmail.com>
2026-01-20 21:04:00 -0500

    initial commit

--- src/main/java/com/ecommercebackend/EcommerceBackEndApplication.java
+++ src/main/java/com/ecommercebackend/EcommerceBackEndApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackEndApplication.class, args);
+    }
 
 }
--- src/test/java/com/ecommercebackend/EcommerceBackEndApplicationTests.java
+++ src/test/java/com/ecommercebackend/EcommerceBackEndApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcommerceBackEndApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
