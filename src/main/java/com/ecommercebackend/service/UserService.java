9ab678bbc1a4abbc1600b1405fe8e01559e22f44
Mumina <muminaabdi13@gmail.com>
2026-01-27 21:55:37 -0500

    - Product entity created - User entity created - AWS S3 connected - AWS RDS (db) connected - Minor changes

--- src/main/java/com/ecommercebackend/service/UserService.java
+++ src/main/java/com/ecommercebackend/service/UserService.java
@@ -5,7 +5,7 @@ import com.ecommercebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+//import java.util.List;
 import java.util.Optional;
 
 @Service
